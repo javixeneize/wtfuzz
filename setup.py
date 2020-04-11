d77431618cd934a7c25f6a78514f76e8232e62d7
JaviDR <javi.xeneize@gmail.com>
2020-04-11 21:08:51 +0100

    changed whatthefuzz folder refence

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description="Web fuzzer",
     url="https://github.com/javixeneize/wtfuzz",
     author="Javier Dominguez",
-    packages=["wtfuzz"],
+    packages=["whatthefuzz"],
     include_package_data=True,
     install_requires=["requests==2.22.0"]
 )
