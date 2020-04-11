fbed2cc9a72f5a99b68eb1d7ca92abeda7e29ca4
JaviDR <javi.xeneize@gmail.com>
2020-04-11 19:48:57 +0100

    no message

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from setuptools import setup
 
-requirements = ['requests==2.22.0',
-'behave==1.2.6']
+requirements = ['requests==2.22.0',]
 setup(
     install_requires=requirements,
     author='Javier Dominguez',
