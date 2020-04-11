cd17efa7840261e45857c6ee8077c02be8f3665b
JaviDR <javi.xeneize@gmail.com>
2020-04-11 19:56:05 +0100

    no message

--- wtfuzz/wtfuzz_core.py
+++ wtfuzz/wtfuzz_core.py
@@ -172,5 +172,6 @@ class wtfuzz():
             else:
                 print('Url/Payloads not correctly initialised')
         return report
-    def test(self): 
+
+    def test(self):
         pass
