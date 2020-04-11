85c400886b012a11a72d92d55d226722c035a8b6
JaviDR <javi.xeneize@gmail.com>
2020-04-11 20:42:05 +0100

    renamed to whatthefuzz

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 
 # This call to setup() does all the work
 setup(
-    name="wtfuzz",
+    name="whatthefuzz",
     version="0.1.0",
     description="Web fuzzer",
     url="https://github.com/javixeneize/wtfuzz",
     author="Javier Dominguez",
-    packages=["wtfuzz"],
+    packages=["whatthefuzz"],
     include_package_data=True,
     install_requires=["requests==2.22.0"]
 )
