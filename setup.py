9ddc25ea034af8ebefe2272fbf13d1a4f1e82631
JaviDR <javi.xeneize@gmail.com>
2020-05-19 12:24:57 +0100

    no message

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="whatthefuzz",
-    version="0.1.4",
+    version="0.1.2",
     description="Web fuzzer",
     url="https://github.com/javixeneize/wtfuzz",
     author="Javier Dominguez",
