c52a44741ce0f3d01e2f659235e63bf14230f0ce
JaviDR <javi.xeneize@gmail.com>
2020-06-03 15:05:18 +0100

    Bump version: 0.2.2 → 0.2.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="whatthefuzz",
-    version="version=0.2.2",
+    version="version=version='0.2.3'",
     description="Web fuzzer",
     url="https://github.com/javixeneize/wtfuzz",
     author="Javier Dominguez",
