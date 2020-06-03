8c5996ea43606ef0bb56a6ff968b100a24e6f5e7
JaviDR <javi.xeneize@gmail.com>
2020-06-03 15:06:36 +0100

    added changes to bumpversion

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="whatthefuzz",
-    version="version=version='0.2.3'",
+    version="version = 0.2.2",
     description="Web fuzzer",
     url="https://github.com/javixeneize/wtfuzz",
     author="Javier Dominguez",
